e848acd1446b0eb63b686f77dbd7bb6f64a6020b
Leandro1308 <remunerando@hotmail.com>
2026-09-13 21:31:25 -0300

    Corrigir conexão TON restaurada antes do login Web

---
+++ ton-connect-session-recovery.js
@@ -0,0 +1,23 @@
+(function(){
+  const lib=window.TON_CONNECT_UI;
+  const Original=lib?.TonConnectUI;
+  if(!Original||Original.__educashproRecoveryPatched)return;
+
+  function hasFreshProof(wallet){
+    const item=wallet?.connectItems?.tonProof;
+    return !!(item&&typeof item==="object"&&item.proof);
+  }
+
+  class EduCashProTonConnectUI extends Original{
+    constructor(options){
+      super(options);
+      const restored=this.wallet;
+      if(restored&&!hasFreshProof(restored)&&typeof this.disconnect==="function"){
+        Promise.resolve(this.disconnect()).catch(()=>{});
+      }
+    }
+  }
+
+  EduCashProTonConnectUI.__educashproRecoveryPatched=true;
+  lib.TonConnectUI=EduCashProTonConnectUI;
+})();
