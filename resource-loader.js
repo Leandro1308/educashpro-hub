13bf4a9a0df049dd423f86059aba676a4cb90320
Leandro1308 <remunerando@hotmail.com>
2026-09-21 18:06:15 -0300

    Corrige encaixe das peças do EduCash Empire

--- resource-loader.js
+++ resource-loader.js
@@ -1,7 +1,7 @@
 (function(){
   "use strict";
 
-  const VERSION="20260921.11";
+  const VERSION="20260921.12";
   const ASSET_TIMEOUT_MS=8000;
   const scripts=new Map();
   const styles=new Map();
