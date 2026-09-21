08b4c935f691a73250e4637f8d717fdb66ef9455
Leandro1308 <remunerando@hotmail.com>
2026-09-21 17:54:21 -0300

    Corrige abertura do EduCash Empire

--- resource-loader.js
+++ resource-loader.js
@@ -1,7 +1,7 @@
 (function(){
   "use strict";
 
-  const VERSION="20260921.10";
+  const VERSION="20260921.11";
   const ASSET_TIMEOUT_MS=8000;
   const scripts=new Map();
   const styles=new Map();
