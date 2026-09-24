63f61a88b132ce92dc027b01d0cf5d97905e7a43
Leandro1308 <remunerando@hotmail.com>
2026-09-24 08:00:41 -0300

    Refresh cache after My Area freeze fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BUILD="2026.09.24.1";
-const CACHE_VERSION="educashpro-pwa-20260924.1";
+const BUILD="2026.09.24.2";
+const CACHE_VERSION="educashpro-pwa-20260924.2";
 
 self.addEventListener("install",()=>self.skipWaiting());
 
